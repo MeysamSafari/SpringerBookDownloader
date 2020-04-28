f54f9d81565a75ae7f29ab05739dbf1d5d6d95e2
Meysam Safari <meysamsafari@yahoo.com>
2020-04-27 23:18:19 -0400

    Second commit: Implementation with function and multi-thread feature

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
 import os
 import re
+import threading
+from concurrent.futures import as_completed
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup as bs
 from time import gmtime, strftime
+from pathlib import Path
 
-filePath = r'C:\Users\meysam\Meysam Test Codes\SpringerBookDownloader\web_scraping_springer_free_books_url.txt'
-folderLocation = r'C:\Users\meysam\Meysam Test Codes\SpringerBookDownloader\DownloadedBooks'
+lock = threading.Lock()
 
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-with open(filePath, encoding='UTF-8') as file:
-    for line in file:
-        #print(line)
-        urls = re.findall('http://[^\s]+', line)
-        if urls != []:
-            #print(urls)
-            r = requests.get(urls[0])
-            #print('URL content:\n', r.content)
-            redirectURL = r.url
-            #print('redirect URL: ', redirectURL)
-            bookID = r.url.rsplit('/', 1)[1]
-            #print('bookID:', bookID)
-            soup = bs(r.content, 'lxml')
-            pageTitle = soup.select_one('title').text
-            #print('pageTitle: ', pageTitle)
-            pageTitleFileName = pageTitle.rsplit(' |', 1)[0].replace(' ', '_').replace('/', '_') + '.pdf'
-            #print('pageTitleFileName: ', pageTitleFileName)
-            fileName = os.path.join(folderLocation, pageTitleFileName)
-            #print('fileName: ', fileName)
-            with open(fileName, 'wb') as f:
-                downloadURL = urljoin("https://link.springer.com/content/pdf/", bookID)
-                #print('downloadURL: ', downloadURL)
-                f.write(requests.get(downloadURL + '.pdf').content)
-                print('Enjoy reading the', pageTitle.rsplit(' |', 1)[0] , 'It is downloaded successfully!')
+def downloader(url: str, folder_name: Path) -> Path:
+    with lock:
+        print("downloading:", url, "\n\n")
+    r = requests.get(url)
+    book_id = r.url.rsplit('/', 1)[1]
+    soup = bs(r.content, 'lxml')
+    page_title = soup.title.text
+    page_title_file_name = page_title.rsplit(' |', 1)[0].replace(' ', '_').replace('/', '_') + '.pdf'
+    result_file = os.path.join(folder_name, page_title_file_name)
+    download_url = urljoin("https://link.springer.com/content/pdf/", book_id)
+    with open(result_file, 'wb') as f:
+        f.write(requests.get(download_url + '.pdf').content)
 
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    return result_file
+
+
+def main():
+    file_name = Path(input("Enter file name with list of book names and their URLs ..."))
+    if not file_name.exists():
+        raise FileNotFoundError(f"{str(file_name)} does't exist")
+    folder_name = Path(input('Enter the folder name to be created to store all pdf files ...'))
+    folder_name.mkdir(parents = True, exist_ok = True)
+
+    print('Start time:', strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+
+    url_pattern = re.compile('http://[^\s]+')
+    urls = url_pattern.findall(file_name.read_text(encoding = 'UTF8'))
+
+    with ThreadPoolExecutor(max_workers = 10) as pool:
+        jobs = {pool.submit(downloader, url, folder_name) for url in urls}
+        for job in as_completed(jobs):
+            result_file = job.result()
+            with lock:
+                print(f"Enjoy reading the {result_file.name}. It is downloaded successfully!!!")
+                print("File available at:", str(result_file.absolute()), "\n\n")
+
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+
+
+if __name__ == '__main__':
+    main()
